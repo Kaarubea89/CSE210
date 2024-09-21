141c38b4ce1a5bbf9bc3e40c9ff383be5ae45edb
Kaarubea89 <karl.ribz.da.great@gmail.com>
2024-09-21 22:56:16 +1200

    first and last name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your First Name?");
+        string  first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your Last Name?");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Hello {first_name} {last_name}!");
+
     }
 }
